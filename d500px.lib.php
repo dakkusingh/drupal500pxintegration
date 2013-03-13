ada405724d9d07fe05479ffc7cafe557d9c6dc12
Dakku <dakku78@gmail.com>
2013-03-13 17:37:31 +0000

    updating the OAuth lib for 500px

--- d500px.lib.php
+++ d500px.lib.php
@@ -4,3 +4,28 @@
  * Exception handling class.
  */
 class D500pxException extends Exception {}
+
+
+
+/**
+ * Primary 500px API implementation class
+ */
+class D500px {
+  /**
+   * @var $source the 500px api 'source'
+   */
+  protected $source = 'drupal';
+  protected $signature_method;
+  protected $consumer;
+  protected $token;
+
+
+
+
+
+
+
+
+
+  
+}
