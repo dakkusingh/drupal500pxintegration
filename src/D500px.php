ae300b3a24fb5056137b6a0f65241c7fbe4149d3
Dakku <dakku@97634.no-reply.drupal.org>
2016-04-29 14:48:22 +0100

    minor cleanup

--- src/D500px.php
+++ src/D500px.php
@@ -18,8 +18,10 @@ class D500px {
    */
   public function __construct($consumer_key, $consumer_secret, $oauth_token = NULL, $oauth_token_secret = NULL) {
     $this->config = \Drupal::config('d500px.settings');
+
     $this->signature_method = new OAuthSignatureMethod_HMAC_SHA1();
     $this->consumer = new OAuthConsumer($consumer_key, $consumer_secret);
+
     if (!empty($oauth_token) && !empty($oauth_token_secret)) {
       $this->token = new OAuthConsumer($oauth_token, $oauth_token_secret);
     }
@@ -33,7 +35,6 @@ class D500px {
   }
 
   public function getRequestToken() {
-    $config = $this->config;
     $url = $this->request_token_url;
 
     try {
